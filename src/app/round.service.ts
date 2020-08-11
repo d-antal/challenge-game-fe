5fb1103092863a4cd6666c4864c3e4cabf132842
AntalDutka <antaldutka@gmail.com>
2020-08-11 19:15:31 +0200

    Round service

---
+++ src/app/round.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoundService {
+
+  private baseUrl = 'http://localhost:8080/lottoland/rock-paper-scissors/api/v1/games';
+
+  constructor(private http: HttpClient) { }
+
+  createRound(gameId: number, value: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/${gameId}/rounds`, value);
+  }
+
+  getRoundsByGame(gameId: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${gameId}/rounds`);
+  }
+
+  getResult(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/rounds/total`);
+  }
+
+}
